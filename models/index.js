37f02d4286ab759ec528d8b0cac98e1cbd7e0571
Ian F <ian.ferg36@gmail.com>
2021-04-28 10:07:00 -0600

    started up server

--- models/index.js
+++ models/index.js
@@ -1,23 +1,25 @@
-const User = require('./User');
-const Workout = require('./Workout');
-const WorkoutHistory = require('./WorkoutHistory');
+const User = require("./User");
+const Workout = require("./Workout");
+const WorkoutHistory = require("./WorkoutHistory");
 
 User.hasMany(Workout, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 Workout.belongsTo(User, {
-    foreignKey: 'owner_id'
+  foreignKey: "owner_id",
 });
 
-User.hasMany(WorkoutHistory, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+User.belongsToMany(WorkoutHistory, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+  through: "UserWorkoutHistory",
 });
 
 WorkoutHistory.belongsToMany(User, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
+  through: "UserWorkoutHistory",
 });
 
 module.exports = { User, Workout, WorkoutHistory };
