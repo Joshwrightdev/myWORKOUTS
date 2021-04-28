52bd45046feb506ce5516b464e45a512fa451151
Michael Knowlton <mknowlton89@gmail.com>
2021-04-28 11:16:23 -0400

    Added the models and the homeRoutes.js file

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { User, Workout, WorkoutHistory } = require('../models');
+const withAuth = require('../utils/auth');
+
+// TODO: Add the homepage route here (So when you go to localhost:3)
+
+
+
+// TODO: Add the login route here
+
+
+
+
+// TODO: Add the signup route here
+
+
+
