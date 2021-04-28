9708d631383e399e87ef0fe1ff1c9862386c12a5
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-04-28 14:15:01 -0400

    created homepage.handlebars and added homepage route to homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,7 +3,11 @@ const { User, Workout, WorkoutHistory } = require("../models");
 const withAuth = require("../utils/auth");
 
 // TODO: Add the homepage route here (So when you go to localhost:3001/ it renders the homepage)
-
+router.get("/", (req, res) => {
+  res.render("homepage", {
+    
+  });
+});
 // TODO: Add the login route here (So when you go to localhost:3001/login it renders the login)
 
 // TODO: Add the signup route here (So when you go to localhost:3001/signup it renders the signup)
@@ -11,7 +15,7 @@ const withAuth = require("../utils/auth");
 // TODO: Add the workouts page here (So when you go to localhost:3001/workouts it renders the workouts page)
 router.get("/", (req, res) => {
   res.render("createworkout", {
-    test: "Create your workout",
+    
   });
 });
 module.exports = router;
