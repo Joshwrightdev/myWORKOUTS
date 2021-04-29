84ac2bea1578c9ae71bdff6728a239b51cfc8f11
Ian F <ian.ferg36@gmail.com>
2021-04-29 11:05:50 -0600

    added more to createworkout.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,9 +4,7 @@ const withAuth = require("../utils/auth");
 
 // TODO: Add the homepage route here (So when you go to localhost:3001/ it renders the homepage)
 router.get("/", (req, res) => {
-  res.render("homepage", {
-    
-  });
+  res.render("homepage", {});
 });
 // TODO: Add the login route here (So when you go to localhost:3001/login it renders the login)
 router.get("/login", (req, res) => {
@@ -17,7 +15,6 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
-
 module.exports = router;
 
 // TODO: Add the signup route here (So when you go to localhost:3001/signup it renders the signup)
@@ -26,9 +23,7 @@ module.exports = router;
 
 module.exports = router;
 // TODO: Add the workouts page here (So when you go to localhost:3001/workouts it renders the workouts page)
-router.get("/", (req, res) => {
-  res.render("createworkout", {
-    
-  });
+router.get("/createworkout", (req, res) => {
+  res.render("createworkout", {});
 });
 module.exports = router;
--- public/js/createworkout.js
+++ public/js/createworkout.js
@@ -1,11 +1,35 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
+  const title = document.getElementById("workout-title").value.trim();
+  const desc = document.getElementById("workout-description").value.trim();
+  const rating = document.getElementById("workout-rating").value.trim();
+  const type = document.getElementById("workout-type").value.trim();
+  const zone = document.getElementById("body-zone").value.trim();
+  const duration = document.getElementById("workout-duration").value.trim();
+  const owner = document.getElementById("owner-id").value.trim();
 
-const title = document.getElementById("workout-title").nodeValue.trim();
-const desc = document.getElementById("workout-description").nodeValue.trim();
-const rating = document.getElementById("workout-rating").nodeValue.trim();
-const type = document.getElementById("workout-type").nodeValue.trim();
-const zone = document.getElementById("body-zone").nodeValue.trim();
-const duratioin = document.getElementById("workout-duration").nodeValue.trim();
-const owner = document.getElementById("owner-id").nodeValue.trim();
+  if (title && desc && rating && type && zone && duration && owner) {
+    const response = await fetch("/createworkout", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        desc,
+        rating,
+        type,
+        zone,
+        duration,
+        owner,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      document.location.replace("/createworkout");
+    } else {
+      alert("Failed to create project");
+    }
+  }
+};
+document.querySelector("").addEventListener("submit", newFormHandler);
