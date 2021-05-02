f49b977c7264bb80081b85a661d2343495e1c675
Michael Knowlton <mknowlton89@gmail.com>
2021-05-02 19:28:31 -0400

    Updated the homepage.handlebars so it greets the user. Also set the homepage route up so if someone tried to hit the homepage without being logged in, it will redirect them to the login page.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,11 +3,25 @@ const { User, Workout, WorkoutHistory } = require("../models");
 const withAuth = require("../utils/auth");
 
 // TODO: Add the homepage route here (So when you go to localhost:3001/ it renders the homepage)
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+
+  // console.log(req.session);
+
+  const userData = await User.findOne({ where: { id: req.session.user_id } });
+
+  const user = userData.get({ plain: true });
+
+  console.log(user);
   res.render("homepage", {
     loggedIn: req.session.logged_in,
+    user
   });
 });
+
 // TODO: Add the login route here (So when you go to localhost:3001/login it renders the login)
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
